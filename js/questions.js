e9b632ed533ddd7a83d15af400e56da4a4fd5714
Raymond Duru <40525180@live.napier.ac.uk>
2022-04-29 00:15:39 -0500

    quiz master v2

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which player scored the fastest hat-trick in the Premier League?",
+    answer: "Sadio Mane 2 minutes 56 seconds",
+    options: [
+      "Sadio Mane 2 minutes 56 seconds",
+      "Ole Gunnar Solskjaer 10 minutes",
+      "Ian Wright  9 minutes",
+      "Robbie Fowler  4 minutes 33 seconds"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When was the first Premier League played?",
+    answer: "1992/93",
+    options: [
+      "1992/93",
+      "1954/55",
+      "1998/99",
+      "1920/21"
+    ]
+  },
+    {
+    numb: 3,
+    question: " In which year did Ronaldo  retire before changing his decision?",
+    answer: "2016",
+    options: [
+      "2020",
+      "2010",
+      "2016",
+      "2007"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which team  won the first Premier League?",
+    answer: "Manchester United",
+    options: [
+      "Chelsea",
+      "Liverpool",
+      "Bolton",
+      "Manchester United"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which country won the first World Cup Tournament?",
+    answer: "Uruguay",
+    options: [
+      "Spain",
+      "France",
+      "England",
+      "Uruguay"
+    ]
+  },
+];
